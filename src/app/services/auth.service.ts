b76b02e26565aa0a71e0971e06548935c8a820ef
ashutosh-k <ashutoshs@dewsolutions.in>
2021-05-19 16:21:34 +0530

    complete tutorial-27f

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from "@angular/core";
 import { Observable, of } from "rxjs";
+import { AuthResponseData } from "../models/AuthResponseData.model";
+import { User } from "../models/user.model";
 
-const loginSuccessDetail = (email: string): any => {
+const loginSuccessDetail = (email: string): AuthResponseData => {
     return {
-        displayName: '',
         email: email,
         expiresIn: "3600",
         idToken: 'kjshdifuhsdfsdfsdfdasfaf',
-        kind: 'sdfhksdhfldsf',
         localId: 'sdkjfyoiuashdfsahdoifhsdf',
         refreshToken: 'sdfafasdfkjasdfh ksa hfdhsafd',
         registered: true
@@ -19,7 +19,13 @@ const loginSuccessDetail = (email: string): any => {
 })
 export class AuthService {
 
-    login(email: string, password: string): Observable<any> {
+    login(email: string, password: string): Observable<AuthResponseData> {
         return of(loginSuccessDetail(email));
     }
+
+    formatUser(data: AuthResponseData) {
+        const expirationDate = new Date(new Date().getTime() + +data.expiresIn * 1000)
+        const user = new User(data.email, data.idToken, data.localId, expirationDate);
+        return user;
+      }
 }
